671e4a82481066466be6ee27b00f0ff8c7a16347
Cezary Dendek <cezary.dendek@gmail.com>
2016-09-24 03:21:08 +0200

    minor fixes, removal of the obsolete code

--- R/glmnet.R
+++ R/glmnet.R
@@ -73,108 +73,6 @@ glmnet.process.output <- function(glmnet.res, nonzero.coeffs.only = FALSE, struc
 }
 
 
-#' @export
-glmnet.path.inner <- function(df.xy, y.cols, ...){
-  xy.col.names <- names(df.xy)
-  x.col.idx <- !(xy.col.names %in% y.cols)
-  mat.y <- df.xy %>%
-    dplyr::select_(.dots = y.cols) %>%
-    as.matrix()
-  df.x <- df.xy[, x.col.idx]
-  df.x.res <- df.as.matrix.dummy(df.x)
-  mat.x <- df.x.res$mat.x
-
-  glmnet.res <- glmnet::glmnet(x = mat.x, y = mat.y, ...)
-
-  glmnet.out <- glmnet.process.output(glmnet.res)
-  coeff.mtx <- coef(glmnet.res)
-  #crit.steps.idx <- structural.changes.col.matrix(coeff.mtx, strict = FALSE)
-
-
-}
-
-
-
-
-#' Data frame to matrix conversion
-#' @export
-tmp.glmnet.path.inner <- function(df.xy, y.cols, ...){
-  # mat.y <- df.xy %>%
-  #   dplyr::select(time = surv.time, status = surv.event) %>%
-  #   as.matrix()
-  xy.col.names <- names(df.xy)
-  x.col.idx <- (xy.col.names %nin% y.cols)
-  mat.y <- df.xy %>%
-    dplyr::select_(.dots = y.cols) %>%
-    as.matrix()
-  df.x <- df.xy[, x.col.idx]
-  mat.x <- model.matrix(~. - 1, data = df.x)
-  # no intercept, each level encoded
-  mat.x.assign <- attr(mat.x, "assign")
-  #mat.x <- mat.x[, mat.x.assign != 0]
-  #mat.x.assign <- mat.x.assign[mat.x.assign != 0]
-  res.translator <- data.frame(
-    out.col = 1:length(mat.x.assign),
-    in.col = mat.x.assign,
-    in.col.name = names(df.x)[mat.x.assign],
-    out.col.name = colnames(mat.x),
-    stringsAsFactors = FALSE
-  )
-
-  tmp.fit <- glmnet::glmnet(mat.x, mat.y, ...)
-
-  ttmp <- coef(tmp.fit) %>% as.matrix()
-  ttmp.df <- t(ttmp) %>% as.data.frame()
-  ttmp.df.n <- names(ttmp.df)
-  col.trans <- data.frame(
-    out.col.name = ttmp.df.n,
-    out.col.name.idx = 1:length(ttmp.df.n),
-    stringsAsFactors = FALSE
-  ) %>%
-    dplyr::inner_join(res.translator, by = "out.col.name") %>%
-    dplyr::arrange(out.col.name.idx)
-  names(ttmp.df) <- col.trans$in.col.name
-  ttmp.df$rowid <- 1:nrow(ttmp.df)
-  ttmp.df.long <- tidyr::gather(
-    data = ttmp.df,
-    key = "variable",
-    value = "value",
-    -rowid
-  ) %>%
-    dplyr::filter(value != 0) %>%
-    dplyr::mutate(value = 1)
-
-  ttmp.df2 <- tidyr::spread(
-    data = ttmp.df.long,
-    key = "variable",
-    value = "value",
-    fill = 0
-  )
-
-  ttmp.df3 <- ttmp.df2 %>%
-    dplyr::select(-rowid) %>%
-    dplyr::distinct() #%>% dplyr::mutate(grpid = 1:n())
-  ttmp.df3
-}
-
-tmp.glmnet.path <- function(model.data.xy){
-  #model.data.xy <- models.data$pts.data.to.fit.os # !!
-  model.data.xy.anyna <- any(is.na(model.data.xy))
-  df.xy <- model.data.xy[complete.cases(model.data.xy), ]
-  zz1 <- tmp.glmnet.path.inner(df.xy = df.xy, family = "cox")
-  if (model.data.xy.anyna) {
-    cc0 <- apply(model.data.xy, 2, function(x) all(!is.na(x)))
-    df.xy2 <- model.data.xy[, cc0]
-    zz2 <- tmp.glmnet.path.inner(df.xy = df.xy2)
-    zz3 <- dplyr::bind_rows(zz1, zz2)
-    zz3[is.na(zz3)] <- 0
-    zz4 <- zz3 %>% dplyr::distinct()
-  } else {
-    zz4 <- zz1
-  }
-  zz4
-}
-
 #' Extraction of critical steps from Lasso path
 #'
 #' Function extracts the steps of the LASSO path resulting in the structural changes of
@@ -234,7 +132,7 @@ structural.changes.col.matrix <- function(mtx, initial.substeps = TRUE, final.su
 #' Function extracts the points of the structure change (adding/dropping coefficient)
 #' as well as the preceeding ones, along with their deviance
 #'
-#' @param model.full.lasso the model fitted by glmnet::glmnet function
+#' @param model.full.lasso the model fitted by \code{\link[glmnet]{glmnet}} function
 #' @param strict if \code{TRUE} only the points of the structural change will be returned
 #' @export
 inference.lasso.path.structural.changes <- function(model.full.lasso, strict = FALSE){
